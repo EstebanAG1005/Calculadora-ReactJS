896bc450d2f6032215b75fd6b88dbae7261f6997
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-05-22 22:39:49 -0600

    Se agrega estructura de tests

---
+++ calculadora/src/components/test/cal.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Calculadora from '../Calculadora';
+
+
